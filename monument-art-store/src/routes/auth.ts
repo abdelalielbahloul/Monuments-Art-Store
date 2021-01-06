146f90b7dc4858d01599b90d5fb05429034fffc5
EL BAHLOUL Abdelali <abdelali.elbahloul@regsquare.com>
2021-01-06 22:05:44 +0100

    feat : add multer to upload user's picture

--- monument-art-store/src/routes/auth.ts
+++ monument-art-store/src/routes/auth.ts
@@ -1,13 +1,42 @@
 import { Router } from "express";
+import * as multer from 'multer';
 import AuthController from "../controller/AuthController";
 import checkJwt from "../middlewars/checkJwt";
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './src/uploads/')
+    },
+    filename: function (req, file, cb) { 
+        cb(null, file.fieldname + '-' + Date.now() + '-' + file.originalname)
+    }
+});
+const fileFilter = (req, file, cb ) => {
+
+    if(file.mimetype === 'image/jpeg' || file.mimetype === 'image/png'){
+        // To accept the file pass `true`, like so:
+        cb(null, true);
+        return;
+    }else{
+        // To reject this file pass `false`, like so:
+        cb(null, false);
+        cb( new Error('The file mime type is not acceptable'))       
+    }
+
+};
+const upload = multer({ 
+    storage: storage, 
+    limits: {
+        fileSize : 1024 * 1024 * 5
+    },
+    fileFilter: fileFilter
+});
 
 const router = Router();
 //Login route
 router.post("/login", AuthController.login);
 
 //register route
-router.post("/register", AuthController.register);
+router.post("/register", upload.single('userImage'), AuthController.register);
 //Change my password
 router.post("/change-password", [checkJwt], AuthController.changePassword);
 
