ff446c1ae2786e674c4cfdae7c62a7a216ee96d3
EL BAHLOUL Abdelali <abdelali.elbahloul@regsquare.com>
2021-01-07 22:51:25 +0100

    feat: add art routes

--- monument-art-store/src/routes/index.ts
+++ monument-art-store/src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import auth from "./auth";
-import user from "./user";
+import auth from "./auth.routes";
+import user from "./user.routes";
+import art from './art.routes'
 
 const routes = Router();
 
 routes.use("/auth", auth);
 routes.use("/users", user);
+routes.use("/arts", art);
 
 export default routes;
