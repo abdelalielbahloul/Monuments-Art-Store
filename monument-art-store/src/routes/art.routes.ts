d056a6d613d420ca2d50182ef4e1de6de797960b
EL BAHLOUL Abdelali <abdelali.elbahloul@regsquare.com>
2021-01-07 22:52:04 +0100

    feat : implement of arts routes

---
+++ monument-art-store/src/routes/art.routes.ts
@@ -0,0 +1,49 @@
+import { Router } from "express";
+import * as multer from 'multer';
+import ArtController from "../controller/ArtController";
+import checkJwt  from "../middlewars/checkJwt";
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './uploads/arts/')
+    },
+    filename: function (req, file, cb) {         
+        cb(null, `${Date.now()}-${file.originalname}`)
+    }
+});
+const fileFilter = (req, file, cb ) => {
+    
+    if(file.mimetype === 'image/jpeg' || file.mimetype === 'image/png'){
+        // To accept the file pass `true`, like so:
+        cb(null, true);
+        return;
+    } else{
+        // To reject this file pass `false`, like so:
+        cb(null, false);
+        cb( new Error('The file mime type is not acceptable!'))       
+    }
+
+};
+const upload = multer({ 
+    storage: storage, 
+    limits: {
+        fileSize : 1024 * 1024 * 5
+    },
+    fileFilter: fileFilter
+});
+
+const router = Router();
+
+//Get all arts
+router.get("/", [checkJwt], ArtController.index);
+
+//Create a new art
+router.post("/", upload.single('artImage'), [checkJwt], ArtController.create);
+
+//Edit one art
+router.patch("/:id", upload.single('artImage'), [checkJwt], ArtController.edit);
+
+//Delete one art
+router.delete("/:id", [checkJwt], ArtController.delete);
+
+export default router;
