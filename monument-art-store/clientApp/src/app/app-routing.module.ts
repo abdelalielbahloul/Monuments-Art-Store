9595e76c6ba509a6ffb47e19296caabd4146cc8f
EL BAHLOUL Abdelali <abdelali.elbahloul@regsquare.com>
2021-01-13 01:55:48 +0100

    refactor : routes array

--- monument-art-store/clientApp/src/app/app-routing.module.ts
+++ monument-art-store/clientApp/src/app/app-routing.module.ts
@@ -1,26 +1,14 @@
 import { RegisterComponent } from './components/register/register.component';
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
-import { ListArtComponent } from './components/art/list-art/list-art.component';
-import { AddArtComponent } from './components/art/add-art/add-art.component';
-import { EditArtComponent } from './components/art/edit-art/edit-art.component';
-import { AddUserComponent } from './components/user/add-user/add-user.component';
-import { EditUserComponent } from './components/user/edit-user/edit-user.component';
-import { ListUserComponent } from './components/user/list-user/list-user.component';
 import { LoginComponent } from './components/login/login.component';
+import { ArtsComponent } from './components/arts/arts.component';
+import { UsersComponent } from './components/users/users.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/arts', pathMatch: 'full'},
-  { path: 'arts', children: [
-    { path: '', component: ListArtComponent },
-    { path: 'create', component: AddArtComponent },
-    { path: 'edit/:id', component: EditArtComponent }
-  ]},
-  { path: 'users', children: [
-    { path: '', component: ListUserComponent },
-    { path: 'create', component: AddUserComponent },
-    { path: 'edit/:id', component: EditUserComponent }
-  ]},
+  { path: 'arts', component: ArtsComponent },
+  { path: 'users', component: UsersComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent }
 ]
