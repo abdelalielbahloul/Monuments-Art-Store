68f4860c8257f91fb327d5b27b82578afc5ca098
EL BAHLOUL Abdelali <abdelali.elbahloul@regsquare.com>
2021-01-14 23:36:30 +0100

    feat(clientApp) : add login form with validation

--- monument-art-store/clientApp/src/app/components/login/login.component.ts
+++ monument-art-store/clientApp/src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  hide = true;
+  loginForm = new FormGroup({
+    login: new FormControl(null, [
+      Validators.required, 
+      Validators.email,
+      Validators.minLength(12)
+    ]),
+    password: new FormControl(null, [
+      Validators.required, 
+      Validators.minLength(8), 
+      Validators.pattern(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#\$%\^&\*])(?=.{8,}).*$/)
+    ])
+  })
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+    console.log(this.loginForm.value);
+    
   }
 
 }
