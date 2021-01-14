09fa5fcf1940fb4331233275aa3c94f92e5c170d
EL BAHLOUL Abdelali <abdelali.elbahloul@regsquare.com>
2021-01-14 23:36:58 +0100

    import http client module and reactive form module

--- monument-art-store/clientApp/src/app/app.module.ts
+++ monument-art-store/clientApp/src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -53,6 +56,8 @@ const materialModules = [
     AppRoutingModule,
     BrowserAnimationsModule,
     ...materialModules,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
