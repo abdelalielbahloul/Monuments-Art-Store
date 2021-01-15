eba8093e10aa8a3c7e434bb1d9ad797f8fe98fcf
EL BAHLOUL Abdelali <abdelali.elbahloul@regsquare.com>
2021-01-15 22:02:59 +0100

    refactor(clientApp) : remove material and install bootstrap

--- monument-art-store/clientApp/src/app/app.module.ts
+++ monument-art-store/clientApp/src/app/app.module.ts
@@ -1,24 +1,13 @@
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatCardModule } from '@angular/material/card';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
 import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatListModule } from '@angular/material/list';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
 
 
 import { LoginComponent } from './components/login/login.component';
@@ -27,21 +16,7 @@ import { NotFoundComponent } from './components/not-found/not-found.component';
 import { UsersComponent } from './components/users/users.component';
 import { ArtsComponent } from './components/arts/arts.component';
 import { ToastrModule } from 'ngx-toastr';
-
-const materialModules = [
-  MatGridListModule,
-  MatCardModule,
-  MatMenuModule,
-  MatIconModule,
-  MatButtonModule,
-  MatSidenavModule,
-  LayoutModule,
-  MatToolbarModule,
-  MatDividerModule,
-  MatListModule,
-  MatFormFieldModule,
-  MatInputModule
-]
+import { JwtInterceptor } from './interceptors/jwt.interceptor';
 
 @NgModule({
   declarations: [
@@ -56,7 +31,6 @@ const materialModules = [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    ...materialModules,
     HttpClientModule,
     ReactiveFormsModule,
     ToastrModule.forRoot({
@@ -64,7 +38,11 @@ const materialModules = [
       preventDuplicates: true,
     })
   ],
-  providers: [],
+  providers: [{
+    provide: HTTP_INTERCEPTORS,
+    useClass: JwtInterceptor,
+    multi: true
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
