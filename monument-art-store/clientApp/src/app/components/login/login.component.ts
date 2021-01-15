42f12730c249d05342e55784f7197010ca769eb9
EL BAHLOUL Abdelali <abdelali.elbahloul@regsquare.com>
2021-01-15 01:33:27 +0100

    feat(clientApp): manipulate token in local storage

--- monument-art-store/clientApp/src/app/components/login/login.component.ts
+++ monument-art-store/clientApp/src/app/components/login/login.component.ts
@@ -1,8 +1,10 @@
+import { TokenService } from './../../services/token.service';
 import { error } from '@angular/compiler/src/util';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -27,7 +29,9 @@ export class LoginComponent implements OnInit {
 
   constructor(
     private authService: AuthService,
-    private toastr: ToastrService
+    private toastr: ToastrService,
+    private tokenService: TokenService,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
@@ -35,13 +39,18 @@ export class LoginComponent implements OnInit {
 
   onSubmit() {
     this.authService._login(this.loginForm.value).subscribe(res => {
-      console.log(res);
+      this.handleToken(res)
       this.toastr.success("Congratulations you are logged in", "Success", { timeOut: 3000 })
       
     }, (err: any) => {
       this.toastr.error(err.error.msg, err.statusText, { timeOut: 3000 })
       
     })
+  }
+
+  handleToken(response) {
+    this.tokenService.handle(response);
+    this.router.navigateByUrl('/arts')
   }
 
 }
