343b96afdf104d080323d32cdaa4020eac7c028d
EL BAHLOUL Abdelali <abdelali.elbahloul@regsquare.com>
2021-01-15 04:35:07 +0100

    featclientApp): secure the routes path using auth guard

--- monument-art-store/clientApp/src/app/app-routing.module.ts
+++ monument-art-store/clientApp/src/app/app-routing.module.ts
@@ -1,16 +1,18 @@
+import { AfterAuthGuard } from './guards/after-auth.guard';
 import { RegisterComponent } from './components/register/register.component';
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { LoginComponent } from './components/login/login.component';
 import { ArtsComponent } from './components/arts/arts.component';
 import { UsersComponent } from './components/users/users.component';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
-  {path: '', redirectTo: '/arts', pathMatch: 'full'},
-  { path: 'arts', component: ArtsComponent },
-  { path: 'users', component: UsersComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent }
+  {path: '', redirectTo: '/arts', pathMatch: 'full', canActivate: [AuthGuard]},
+  { path: 'arts', component: ArtsComponent, canActivate: [AuthGuard] },
+  { path: 'users', component: UsersComponent, canActivate: [AuthGuard] },
+  { path: 'login', component: LoginComponent, canActivate: [AfterAuthGuard] },
+  { path: 'register', component: RegisterComponent, canActivate: [AfterAuthGuard] }
 ]
 
 
