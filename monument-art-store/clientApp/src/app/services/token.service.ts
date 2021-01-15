dd4476365132b5f70e4962f8eb8bb89ae5973b64
EL BAHLOUL Abdelali <abdelali.elbahloul@regsquare.com>
2021-01-15 01:19:48 +0100

    feat(clientApp) : token service implementation

--- monument-art-store/clientApp/src/app/services/token.service.ts
+++ monument-art-store/clientApp/src/app/services/token.service.ts
@@ -1,9 +1,80 @@
 import { Injectable } from '@angular/core';
-
+import * as moment from "moment";
 @Injectable({
   providedIn: 'root'
 })
 export class TokenService {
 
   constructor() { }
+
+  set(data: any) {
+    const expiredIn = moment().add(data.expiredIn,'second');
+    localStorage.setItem('token', data.token);
+    localStorage.setItem('expiredIn', JSON.stringify(expiredIn.valueOf()))
+    localStorage.setItem('userId', data._id)
+  }
+
+  handle(data) {
+    this.set(data);
+  }
+
+  getToken() {
+    return localStorage.getItem('token');
+  }
+
+  getUserId() {
+    return localStorage.getItem('userId')
+  }
+
+  getExpiration() {
+    const expiration = localStorage.getItem("expiredIn");
+    const expiredIn = JSON.parse(expiration);
+    return moment(expiredIn);
+}  
+
+  remove() {
+    localStorage.removeItem('token');
+    localStorage.removeItem('expiredIn')
+    localStorage.removeItem('userId')
+  }
+
+  // decode of token payload
+  decode(payload) {
+    return JSON.parse(atob(payload));
+  }
+
+  payload(token) {
+    const payload = token.split('.')[1];
+    return this.decode(payload);
+  }
+
+  // verify if the token is valid
+  isValid() {
+    const token = this.getToken();
+    const userId = this.getUserId()
+    if(token) {
+      const payload = this.payload(token);
+      if(payload) return userId == payload.userId && moment().isBefore(this.getExpiration());
+    }
+    return false
+  }
+
+  getInfos() {
+    const token = this.getToken();
+    if(token) {
+      const payload = this.payload(token);
+      return payload ? payload : null;
+    }
+    return null;
+  }
+
+  isLoggedIn() {
+    return this.isValid();
+  }
+
+  isLoggedOut() {
+    return !this.isLoggedIn();
+  }
+
+
 }
