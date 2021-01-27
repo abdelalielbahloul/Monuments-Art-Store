c8f9ad718a4bb0d67192839a61da531aa1474df8
EL BAHLOUL Abdelali <abdelali.elbahloul@regsquare.com>
2021-01-27 15:48:36 +0100

    feat : admin guard add toastr when unauthorized route clicked

--- monument-art-store/clientApp/src/app/guards/admin.guard.ts
+++ monument-art-store/clientApp/src/app/guards/admin.guard.ts
@@ -1,7 +1,7 @@
+import { ToastrService } from 'ngx-toastr';
 import { TokenService } from './../services/token.service';
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
-import { Observable } from 'rxjs';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -11,14 +11,17 @@ export class AdminGuard implements CanActivate {
   connectedUser = null
   constructor(
     private tokenService: TokenService,
-    private router: Router
+    private router: Router,
+    private toastr: ToastrService
   ) { }
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): boolean {
-    this.connectedUser = this.tokenService.getInfos()
+    this.connectedUser = this.tokenService.getInfos()   
+    
     if(this.connectedUser.role !== 'ADMIN') {
       this.router.navigateByUrl("/");
+      this.toastr.warning('You do not have permission to access this route', 'Unauthorized', { timeOut: 5000 })
       return false
     }
     return true;
