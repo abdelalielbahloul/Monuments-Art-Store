1d15caa1539d4e5293c24af08c6d6216fc890611
EL BAHLOUL Abdelali <abdelali.elbahloul@regsquare.com>
2021-01-28 17:07:05 +0100

    feat : implementation of art service

--- monument-art-store/clientApp/src/app/services/art.service.ts
+++ monument-art-store/clientApp/src/app/services/art.service.ts
@@ -1,9 +1,50 @@
+import { Art } from './../models/Art';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { throwError } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ArtService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  
+  _fetch() {
+    return this.http.get<Art[]>(`${environment.apiUrl}/arts`).pipe(
+      map(data => data),
+      catchError(this.handleError)
+    );
+  }
+
+  _create(data: FormData) {
+    return this.http.post(`${environment.apiUrl}/arts`, data)
+      .pipe(
+        map(res => res),
+        catchError(this.handleError)
+      )
+  }
+
+  _delete(id: string) {
+    return this.http.delete(`${environment.apiUrl}/arts/${id}`)
+      .pipe(
+        map(res => res),
+        catchError(this.handleError)
+      )
+  }
+
+  _update(data: FormData, _id) {
+    return this.http.patch(`${environment.apiUrl}/arts/${_id}`, data)
+      .pipe(
+        map(res => res),
+        catchError(this.handleError)
+      )
+  }
+
+  handleError(error) { 
+    return throwError(error);
+
+  }
 }
